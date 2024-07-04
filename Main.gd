3e12f3d07f67ac59dc95068933f4527ec6982155
HypotheticalWhale <samueltan.9803@gmail.com>
2024-07-04 12:13:08 +0800

    tiles are kinda usable

--- Main.gd
+++ Main.gd
@@ -3,11 +3,12 @@ extends Node2D
 @export var GRID_SIZE = [20,10]
 var tile_path = preload("res://Tile/TileNode.tscn")
 var tile_node
+var tile_coords
+var all_tiles = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	PlayerData.create_units()
 	spawn_tiles()
-	
 	spawn_units()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,13 +18,17 @@ func spawn_units():
 	var count = 0
 	for unit in PlayerData.player1_units.values():
 		add_child(unit)
-		unit.global_position = Vector2(0,count)*Globals.TILE_SIZE
+		tile_coords = Vector2(0,count)*Globals.TILE_SIZE
+		unit.global_position = tile_coords
+		all_tiles[tile_coords].occupied_by = unit
 		count += 1
 		
 	count = 0
 	for unit in PlayerData.player2_units.values():
 		add_child(unit)
-		unit.global_position = Vector2(3,count)*Globals.TILE_SIZE
+		tile_coords = Vector2(3,count)*Globals.TILE_SIZE
+		unit.global_position = tile_coords
+		all_tiles[tile_coords].occupied_by = unit		
 		count += 1
 
 func spawn_tiles():
@@ -31,4 +36,14 @@ func spawn_tiles():
 		for y_tile in range(0,GRID_SIZE[1]):
 			tile_node = tile_path.instantiate()
 			add_child(tile_node)
-			tile_node.global_position = Vector2(x_tile,y_tile)*Globals.TILE_SIZE
+			tile_coords = Vector2(x_tile,y_tile)*Globals.TILE_SIZE
+			tile_node.tile_coordinates = tile_coords
+			tile_node.global_position = tile_coords
+			all_tiles[tile_coords] = tile_node
+
+func get_occupied_tiles():
+	var occupied_tiles = []
+	for tile in all_tiles.values():
+		if tile.occupied_by != null:
+			occupied_tiles.append(tile)
+	return occupied_tiles
