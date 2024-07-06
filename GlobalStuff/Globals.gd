1b55d3e0973953f1ca9adffe1db55092c18131c2
HypotheticalWhale <samueltan.9803@gmail.com>
2024-07-06 11:10:09 +0800

    turn timer and main ui base is complete

--- GlobalStuff/Globals.gd
+++ GlobalStuff/Globals.gd
@@ -1,11 +1,19 @@
 extends Node
 
 const TILE_SIZE = 32
+var WHOSTURNISIT = "P1"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func toggle_player_turn():
+	if WHOSTURNISIT == "P2":
+		WHOSTURNISIT = "P1"
+	else:
+		WHOSTURNISIT = "P2"
+	return
+
