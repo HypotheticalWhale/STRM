88c0f517d00e721661eebeb5f2a4506d1b3c61ef
HypotheticalWhale <samueltan.9803@gmail.com>
2024-07-31 13:42:48 +0800

    u can now attack and kill your enemies/teammates

--- Classes/Servant.gd
+++ Classes/Servant.gd
@@ -2,8 +2,10 @@ extends BaseClass
 
 
 func initialize_stats():
+	MAX_HEALTH = 60
 	CURRENT_HEALTH = 60
 	MOVEMENT = 2
+	DAMAGE = 10
 	ACTIONS = ["Sweep Attack"]
 	QUEST = "fight"
 	POTENTIAL_JOBS = ["gardener", "gardener", "gardener"]
